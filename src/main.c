0a71e90275b6fe2242e0d025f3466cb672576e30
Kevin Xu <23307110122@m.fudan.edu.cn>
2025-09-19 05:15:33 +0000

    This is lab0 dev

--- src/main.c
+++ src/main.c
@@ -6,17 +6,36 @@
 
 static volatile bool boot_secondary_cpus = false;
 
+static void check_bss_zero(void *start, void *end){
+    unsigned char *p   = (unsigned char *)start;
+    long long unsigned int len  = (unsigned char *)end - p;
+    long long unsigned int bad  = 0;
+
+    for (long long unsigned int i = 0; i < len; ++i) {
+        if (p[i] != 0) {
+            ++bad;
+        }
+    }
+    if (bad == 0)
+        printk("BSS check PASSED: %llu bytes are all zero.\n", len);
+    else
+        printk("BSS check FAILED: %llu non-zero bytes found.\n", bad);
+}
+
 void main()
 {
     if (cpuid() == 0) {
         /* @todo: Clear BSS section.*/
+        extern char bss[], ebss[];
+        memset(bss, 0, ebss - bss); 
+        check_bss_zero(bss, ebss);
 
         smp_init();
         uart_init();
         printk_init();
 
         /* @todo: Print "Hello, world! (Core 0)" */
-
+        printk("Hello, world! (Core 0)\n");
         arch_fence();
 
         // Set a flag indicating that the secondary CPUs can start executing.
@@ -27,6 +46,7 @@ void main()
         arch_fence();
 
         /* @todo: Print "Hello, world! (Core <core id>)" */
+        printk("Hello, world! (Core %llu)\n", cpuid());
     }
 
     set_return_addr(idle_entry);
