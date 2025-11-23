776088b5207a98d1102bfe202ad07523635e2afd
Kevin <23307110122@m.fudan.edu.cn>
2025-11-23 17:49:43 +0800

    fin

--- src/fs/test/mock/printk.cpp
+++ src/fs/test/mock/printk.cpp
@@ -1,12 +1,12 @@
-// __asm(".globl printk\n"
-//       "printk:\n"
-//       "b printf");
-#include <cstdio>
-#include <cstdarg>
+__asm(".globl printk\n"
+      "printk:\n"
+      "b printf");
+// #include <cstdio>
+// #include <cstdarg>
 
-extern "C" void printk(const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    vprintf(fmt, args);
-    va_end(args);
-}
+// extern "C" void printk(const char *fmt, ...) {
+//     va_list args;
+//     va_start(args, fmt);
+//     vprintf(fmt, args);
+//     va_end(args);
+// }
