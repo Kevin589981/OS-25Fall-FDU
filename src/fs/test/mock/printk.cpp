5f459ff115be2558476b294197c810f347c341af
Kevin <23307110122@m.fudan.edu.cn>
2025-12-10 00:00:26 +0800

    编写了更多测试

--- src/fs/test/mock/printk.cpp
+++ src/fs/test/mock/printk.cpp
@@ -1,12 +1,11 @@
-// __asm(".globl printk\n"
-//       "printk:\n"
-//       "b printf");
-#include <cstdio>
-#include <cstdarg>
-
-extern "C" void printk(const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    vprintf(fmt, args);
-    va_end(args);
-}
+#if defined(__aarch64___)||defined(__arm__)
+__asm(".globl printk\n"
+      "printk:\n"
+      "b printf");
+#elif defined(__x86_64__)||defined(__i386__)
+__asm__(".globl printk\n"
+        "printk:\n"
+        "jmp printf");
+#else
+#error "Unsupported architecture"
+#endif
